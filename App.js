e50242e2f6870312aa4dec0464584da882cedc56
Mali Michael <malimccalla@gmail.com>
2017-09-16 14:45:13 +0100

    Foursquare call

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { MapView } from 'expo';
+import axios from 'axios';
+import config from './config';
 
 export default class App extends React.Component {
+  getData = async () => {
+    const data = await axios.get(
+      `https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&v=20160601&client_id=VPSWABARGJ4R3K4L0MYHQL3D2DNTU0K1HJGJI4TNVZMTIAKO&client_secret=${config.fsSecret}`
+    );
+    console.log('DATA', data);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput autoFocus style={styles.input}/>
+        <TouchableOpacity onPress={this.getData} style={styles.button}><Text>Load data</Text></TouchableOpacity>
       </View>
     );
   }
@@ -16,8 +25,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7f7f7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 50,
+    width: 300,
+    backgroundColor: '#fff'
+  },
+  button: {
+    height: 50,
+    width: 300,
+    marginTop: 50,
+    backgroundColor: '#e4d100'
+  }
 });
